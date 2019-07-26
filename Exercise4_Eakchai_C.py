5b8adb4dcc9412b86591cfc324503ce2b16b6f96
eakchai8532 <52350483+eakchai8532@users.noreply.github.com>
2019-07-26 16:16:09 +0700

    Add files via upload

---
+++ Exercise4_Eakchai_C.py
@@ -0,0 +1,9 @@
+f=80
+g=70
+i=60
+c=50
+print("------Your Score------")
+print("Foundation English",":",f)
+print("General Business",":",g)
+print("Introduction to Computer",":",i)
+print("Computer Programming",":",c)
