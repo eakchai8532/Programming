c3fa49b819bce1a52c699686849ae4162d890d5d
eakchai8532 <52350483+eakchai8532@users.noreply.github.com>
2019-09-14 19:27:40 +0700

    Add files via upload

---
+++ Test (1).py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1.
+
+# In[ ]:
+
+
+a=float(input())
+b=float(a*7/100)
+area=a+b
+print(area)
+
+
+# # 2
+
+# In[ ]:
+
+
+
+
+
+# # 3
+
+# In[ ]:
+
+
+for x in range(5)
+    print("้Hello")
+
+
+# # 4
+
+# In[ ]:
+
+
+for x in range(12)
+    a = x+1
+    b = 2*x
+    print("2 x",x,"=",b")
+
